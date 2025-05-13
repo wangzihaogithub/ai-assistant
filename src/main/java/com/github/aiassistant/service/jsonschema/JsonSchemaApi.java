70ae1641b35969f950a59ce8574380da692cd26c
wangzihao <wangzihao@cc.cc>
2025-05-13 16:18:46 +0800

    优化代码

--- src/main/java/com/github/aiassistant/service/jsonschema/JsonSchemaApi.java
+++ src/main/java/com/github/aiassistant/service/jsonschema/JsonSchemaApi.java
@@ -11,7 +11,7 @@ import dev.langchain4j.model.chat.request.json.JsonSchema;
  * 当模型不调用工具时，但以结构化方式响应用户时，这非常有用。
  * 此功能适用于我们最新的 GPT-4o 模型：今天发布的 gpt-4o-2024-08-06 和 gpt-4o-mini-2024-07-18。
  * 当提供了 response_format 并设置 strict: true 时，模型输出将匹配提供的架构。
- * https://platform.openai.com/docs/guides/structured-outputs?api-mode=responses&example=structured-data
+ * https://platform.openai.com/docs/guides/structured-outputs
  */
 public interface JsonSchemaApi {
     default JsonSchema getJsonSchema() {
