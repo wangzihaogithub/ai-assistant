02db2d41667e61326475b8c7dec2cea869b189db
wangzihao <842156727@qq.com>
2025-12-23 19:50:59 +0800

    丰富rag操作

--- src/main/java/com/github/aiassistant/service/text/sseemitter/SseHttpResponse.java
+++ src/main/java/com/github/aiassistant/service/text/sseemitter/SseHttpResponse.java
@@ -24,6 +24,7 @@ public interface SseHttpResponse {
      * @param triggerEventEnum 事件类型
      * @param payload          事件数据
      * @param timestamp        事件时间
+     * @param <T>              事件数据类型
      */
     <T> void userTrigger(UserTriggerEventEnum<T> triggerEventEnum, T payload, long timestamp);
 
@@ -32,7 +33,7 @@ public interface SseHttpResponse {
      *
      * @param triggerEventEnum 事件类型
      * @param payload          事件数据
-     * @param <T> 事件数据类型
+     * @param <T>              事件数据类型
      */
     default <T> void userTrigger(UserTriggerEventEnum<T> triggerEventEnum, T payload) {
         userTrigger(triggerEventEnum, payload, System.currentTimeMillis());
